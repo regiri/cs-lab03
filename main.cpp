d8a12e2782aa5abc24e64fe158a1baa28667a7d0
regiri <goparegiri@gmail.com>
2022-04-18 00:59:04 +0300

    added histograme scaling

--- main.cpp
+++ main.cpp
@@ -126,7 +126,7 @@ void svg_text(double left, double baseline, string text){
 }
 
 
-void svg_rect(double x, double y, double width, double height, string stroke, string fill) {
+void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black") {
     cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill='" << fill << "' />\n";
 }
 
@@ -140,11 +140,31 @@ void show_histogram_svg(const vector<size_t>& bins)
     const auto TEXT_WIDTH = 50;
     const auto BIN_HEIGHT = 30;
     const auto BLOCK_WIDTH = 10;
+
+    size_t max_count = bins[0];
+    for (size_t bin : bins)
+    {
+        if (max_count < bin)
+        {
+            max_count = bin;
+        }
+    }
+
+
     svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
     double top = 0;
     for (size_t bin : bins) {
-        const double bin_width = BLOCK_WIDTH * bin;
-        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+        size_t height;
+        if (max_count > MAX_ASTERISK)
+        {
+            height = MAX_ASTERISK * (static_cast<double>(bin) / max_count);
+        }
+        else
+        {
+            height = bin;
+        }
+        const double bin_width = BLOCK_WIDTH * height;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(height));
         svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "red", "red");
         top += BIN_HEIGHT;
     }
