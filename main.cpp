3f5005074f3bb9ada039792cc473a59f3c70f76f
regiri <goparegiri@gmail.com>
2022-04-18 14:09:06 +0300

    added file with all svg functions

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <vector>
 #include "histogram.h"
+#include "svg.h"
 using namespace std;
-
-const size_t SCREEN_WIDTH = 80;
-const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
 
 vector<double> input_numbers(size_t cnt)
 {
@@ -15,9 +13,6 @@ vector<double> input_numbers(size_t cnt)
     }
     return result;
 }
-
-
-
 
 
 vector<size_t> make_histogram(const vector<double>& numbers, const size_t bin_count)
@@ -89,72 +84,7 @@ void show_histogram_text(const vector<size_t>& bins)
 }
 
 
-void svg_begin(double width, double height)
-{
-    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
-    cout << "<svg ";
-    cout << "width='" << width << "' ";
-    cout << "height='" << height << "' ";
-    cout << "viewBox='0 0 " << width << " " << height << "' ";
-    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
-}
 
-
-void svg_end()
-{
-    cout << "</svg>\n";
-}
-
-
-void svg_text(double left, double baseline, string text){
-    cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>\n";
-}
-
-
-void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black") {
-    cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill='" << fill << "' />\n";
-}
-
-
-void show_histogram_svg(const vector<size_t>& bins)
-{
-    const auto IMAGE_WIDTH = 400;
-    const auto IMAGE_HEIGHT = 300;
-    const auto TEXT_LEFT = 20;
-    const auto TEXT_BASELINE = 20;
-    const auto TEXT_WIDTH = 50;
-    const auto BIN_HEIGHT = 30;
-    const auto BLOCK_WIDTH = 10;
-
-    size_t max_count = bins[0];
-    for (size_t bin : bins)
-    {
-        if (max_count < bin)
-        {
-            max_count = bin;
-        }
-    }
-
-
-    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
-    double top = 0;
-    for (size_t bin : bins) {
-        size_t height;
-        if (max_count > MAX_ASTERISK)
-        {
-            height = MAX_ASTERISK * (static_cast<double>(bin) / max_count);
-        }
-        else
-        {
-            height = bin;
-        }
-        const double bin_width = BLOCK_WIDTH * height;
-        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(height));
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "red", "red");
-        top += BIN_HEIGHT;
-    }
-    svg_end();
-}
 
 int main()
 {
