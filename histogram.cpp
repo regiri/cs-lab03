48a7ebd86beebb5e8000f7b1a7c99cdb963bc35b
regiri <goparegiri@gmail.com>
2022-04-18 14:00:23 +0300

    added test for histogram.cpp

---
+++ histogram.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include "histogram.h"
+
+using namespace std;
+
+void find_minmax(const vector<double>& numbers, double& min, double& max)
+{
+    if (numbers.size() == 0) {
+        return;
+    }
+    min = numbers[0];
+    max = numbers[0];
+    for (auto num : numbers)
+    {
+        if (num < min)
+        {
+            min = num;
+        }
+        if (num > max)
+        {
+            max = num;
+        }
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#include "histogram.h"
 using namespace std;
 
 const size_t SCREEN_WIDTH = 80;
@@ -17,22 +17,7 @@ vector<double> input_numbers(size_t cnt)
 }
 
 
-void find_minmax(const vector<double>& numbers, double& min, double& max)
-{
-    min = numbers[0];
-    max = numbers[0];
-    for (auto num : numbers)
-    {
-        if (num < min)
-        {
-            min = num;
-        }
-        if (num > max)
-        {
-            max = num;
-        }
-    }
-}
+
 
 
 vector<size_t> make_histogram(const vector<double>& numbers, const size_t bin_count)
